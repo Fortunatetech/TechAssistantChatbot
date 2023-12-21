8c36867d98c205291c4f10e4ed6158404a54e0c5
Fortunatetech <ayodeleayodeji250@gmail.com>
2023-12-21 10:41:00 +0100

    updated trial.ipynb

--- secret_key.py
+++
@@ -1,3 +0,0 @@
-# openapi_key = "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"
-openapi_key = "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"
-hugging_face_key = "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"
